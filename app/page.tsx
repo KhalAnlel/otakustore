14eabbde1214ae7e293572572added8245cd70a6
Anlel Napleon <78030713+KhalAnlel@users.noreply.github.com>
2024-01-22 22:37:07 +0300

    update home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
-import Home from "./home/page";
+import BestProducts from "./home/bestProducts";
+import HomeSlider from "./home/homeSlider";
+import LogoClouds from "./home/logoClouds";
+import PopularProducts from "./home/popularProducts";
+import ProductOfTheWeek from "./home/productOfTheWeek";
+import Promo from "./home/promo";
+import TopCategories from "./home/topCategories";
 
 export default function app() {
-  return <Home />;
+  return (
+    <div>
+      <HomeSlider />
+      <TopCategories />
+      <BestProducts />
+      <Promo />
+      <PopularProducts />
+      <ProductOfTheWeek />
+      <LogoClouds />
+    </div>
+  );
 }
