5c3501f5afb57152ef6fc156749b785a39c6d318
Anlel Napleon <78030713+KhalAnlel@users.noreply.github.com>
2024-01-25 23:24:01 +0300

    update design of update product

--- app/dashboard/update/[id]/updateForm.tsx
+++ app/dashboard/update/[id]/updateForm.tsx
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form";
 import axios from "axios";
 import colors from "@/app/data/colors";
 import sizes from "@/app/data/sizes";
-import { Button, Input, Select, SelectItem } from "@nextui-org/react";
+import { Button, Input } from "@nextui-org/react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useRouter } from "next/navigation";
@@ -27,7 +27,6 @@ interface ItemForm extends Omit<Props["product"], "id"> {
   color_ids: number[];
   size_ids: number[];
 }
-// ... Other imports
 
 const UpdateForm: React.FC<Props> = ({ product }) => {
   const router = useRouter();
@@ -48,19 +47,17 @@ const UpdateForm: React.FC<Props> = ({ product }) => {
     setSelectedSizes(product.size_ids);
   }, [product, setValue]);
 
-  
+  const isSubmitDisabled =
+    selectedColors.length === 0 || selectedSizes.length === 0;
 
   const onSubmit = async (data: ItemForm) => {
-    // Convert relevant form values to numbers
     data.price = parseInt(data.price as any, 10);
     data.rate = parseInt(data.rate as any, 10);
     data.stock = parseInt(data.stock as any, 10);
-
-    // Update form data with selected color_ids and size_ids
     data.color_ids = selectedColors;
     data.size_ids = selectedSizes;
 
-     toast.success("Item Added successfully.", {
+    toast.success("Item Updated Successfully.", {
       position: "top-right",
       autoClose: 1000,
       hideProgressBar: false,
@@ -69,9 +66,8 @@ const UpdateForm: React.FC<Props> = ({ product }) => {
       draggable: true,
     });
 
-    // Send the data to the API
     await axios.patch(`/api/products/${product.id}`, data);
-    router.push("/dashboard")
+    router.push("/dashboard");
   };
 
   return (
@@ -79,58 +75,141 @@ const UpdateForm: React.FC<Props> = ({ product }) => {
       onSubmit={handleSubmit(onSubmit)}
       className="flex flex-col gap-4 p-10"
     >
-      <Input type="text" variant="bordered" {...register("title")} defaultValue={product.title} label="Title"/>
-      <Input type="text" variant="bordered" {...register("description")} defaultValue={product.description} label="Description"/>
-      <Input type="text" variant="bordered" {...register("price")} defaultValue={product.price.toString()} label="Price"/>
-      <Input type="text" variant="bordered" {...register("rate")} defaultValue={product.rate.toString()} label="Rate"/>
-      <Input type="text" variant="bordered" {...register("stock")} defaultValue={product.stock.toString()} label="Stock"/>
-      <Input type="text" variant="bordered" {...register("type")} defaultValue={product.type} label="Type"/>
-      <Input type="text" variant="bordered" {...register("category")} defaultValue={product.category} label="Category"/>
-       <div>
-        <select
-          data-te-select-init
-          multiple
-          onChange={(e) => {
-            const selectedColorIds = Array.from(
-              e.target.selectedOptions,
-              (option) => parseInt(option.value, 10)
-            );
-            setSelectedColors(selectedColorIds);
-          }}
-          value={selectedColors.map(String)}
-        >
-          {colors.map((color, index) => (
-            <option key={index + 1} value={index + 1}>
-              {color}
-            </option>
-          ))}
-        </select>
-        <label>Select Colors</label>
-      </div>
-      <div>
-        <select
-          data-te-select-init
-          multiple
-          onChange={(e) => {
-            const selectedSizeIds = Array.from(
-              e.target.selectedOptions,
-              (option) => parseInt(option.value, 10)
-            );
-            setSelectedSizes(selectedSizeIds);
-          }}
-          value={selectedSizes.map(String)}
-        >
-          {sizes.map((size, index) => (
-            <option key={index + 1} value={index + 1}>
-              {size}
-            </option>
-          ))}
-        </select>
-        <label>Select Sizes</label>
-      </div>
+      <h1 className="font-bold text-xl text-center text-black">Update Product</h1>
+      <Input
+        type="text"
+        color="danger"
+        variant="bordered"
+        {...register("title")}
+        defaultValue={product.title}
+        label="Title"
+        isRequired
+        className="text-black"
+      />
+      <Input
+        type="text"
+        color="danger"
+        variant="bordered"
+        {...register("description")}
+        defaultValue={product.description}
+        label="Description"
+        isRequired
+        className="text-black"
+      />
+      <Input
+        type="number"
+        color="danger"
+        variant="bordered"
+        {...register("price")}
+        defaultValue={product.price.toString()}
+        label="Price"
+        isRequired
+        min={1}
+        className="text-black"
+      />
+      <Input
+        type="number"
+        color="danger"
+        variant="bordered"
+        {...register("rate")}
+        defaultValue={product.rate.toString()}
+        label="Rate"
+        isRequired
+        min={1}
+        max={5}
+        className="text-black"
+      />
+      <Input
+        type="number"
+        color="danger"
+        variant="bordered"
+        {...register("stock")}
+        defaultValue={product.stock.toString()}
+        label="Stock"
+        isRequired
+        min={1}
+        className="text-black"
+      />
+      <Input
+        type="text"
+        color="danger"
+        variant="bordered"
+        {...register("type")}
+        defaultValue={product.type}
+        label="Type"
+        isRequired
+        className="text-black"
+      />
+      <Input
+        type="text"
+        color="danger"
+        variant="bordered"
+        {...register("category")}
+        defaultValue={product.category}
+        label="Category"
+        isRequired
+        className="text-black"
+      />
+      <label className="font-bold text-black">Select Colors</label>
+      <div className="h-96 overflow-auto flex flex-col gap-1 bg-gray-200 p-2 rounded-lg">
+        {colors.map((color, index) => (
+          <div>
+            <input
+              type="checkbox"
+              key={index + 1}
+              value={color}
+              id={color}
+              checked={selectedColors.includes(index + 1)}
+              onChange={(e) => {
+                const checked = e.target.checked;
+                const colorId = index + 1;
 
-      <Button variant="bordered" className="text-danger hover:bg-danger hover:text-white" type="submit">
-        Submit
+                if (checked) {
+                  setSelectedColors((prevColors) => [...prevColors, colorId]);
+                } else {
+                  setSelectedColors((prevColors) =>
+                    prevColors.filter((id) => id !== colorId)
+                  );
+                }
+              }}
+            />
+            <label htmlFor={color} className="text-black"> {color}</label>
+          </div>
+        ))}
+      </div>
+      <label className="font-bold text-black">Select Sizes</label>
+      <div className="h-48 overflow-auto flex flex-col gap-1 bg-gray-200 p-2 rounded-lg">
+        {sizes.map((size, index) => (
+          <div>
+            <input
+              type="checkbox"
+              key={index + 1}
+              value={size}
+              id={size}
+              checked={selectedSizes.includes(index + 1)}
+              onChange={(e) => {
+                const checked = e.target.checked;
+                const sizeId = index + 1;
+                if (checked) {
+                  setSelectedSizes((prevSizes) => [...prevSizes, sizeId]);
+                } else {
+                  setSelectedSizes((prevSizes) =>
+                    prevSizes.filter((id) => id !== sizeId)
+                  );
+                }
+              }}
+            />
+            <label htmlFor={size} className="text-black"> {size}</label>
+          </div>
+        ))}
+      </div>
+      <Button
+        variant="bordered"
+        className="text-danger hover:bg-danger hover:text-white disabled:cursor-not-allowed disabled:hover:bg-slate-800"
+        type="submit"
+        disabled={isSubmitDisabled}
+      >
+        {!isSubmitDisabled ? "Submit" : "Make Sure You Selected Color or Size"}
       </Button>
     </form>
   );
