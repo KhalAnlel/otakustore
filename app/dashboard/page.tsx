867931c656b2f30b37d365dcdb927c305d54b6d6
Anlel Napleon <78030713+KhalAnlel@users.noreply.github.com>
2024-02-01 17:02:48 +0300

    Revert "Update page.tsx"
    This reverts commit 3f728c49a5f00b1b7c0cf9ca1857c6440650be36.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,10 +8,9 @@ import { redirect } from "next/navigation";
 
 const Dashboard = async () => {
   const session = await getServerSession(authOptions);
-  if (session) {
-    redirect('/dashboard');
+  if (!session) {
+    redirect('/');
   }
-  else  redirect('/');
   const products = await prisma.product.findMany({
     include: {
       ProductColor: true,
