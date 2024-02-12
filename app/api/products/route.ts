1b596656f1bbd6027950c4cb577ee6fd608a56f4
Anlel Napleon <78030713+KhalAnlel@users.noreply.github.com>
2024-02-12 15:45:09 +0300

    Update route.ts

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -90,14 +90,14 @@ export async function POST(request: NextRequest) {
       return NextResponse.json({ error: "Invalid file type" }, { status: 400 });
     }
     
-    // const buffer = Buffer.from(await file.arrayBuffer());
+    const buffer = Buffer.from(await file.arrayBuffer());
     const filename = Date.now() + file.name.replaceAll(" ", "_");
     
     await s3Client.send(
       new PutObjectCommand({
         Bucket: process.env.NEXT_AWS_S3_BUCKET_NAME!,
         Key: filename,
-        Body: file,
+        Body: buffer,
       })
     );
 
