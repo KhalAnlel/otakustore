9bafdc06d2cb67ebb885df9d41fc7a2423fd9ddd
Anlel Napleon <anleelpatto@icloud.com>
2024-03-10 16:40:46 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ["otakustorebucket.s3.eu-north-1.amazonaws.com"]
+      }
+}
 
 module.exports = nextConfig
